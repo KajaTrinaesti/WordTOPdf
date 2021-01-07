e0b772041a0aa6e44007b66bfce62e14da5504af
Dino Karahmetovic <dino.prushka@gmail.com>
2021-01-07 19:17:47 +0100

    s

--- app.js
+++ app.js
@@ -81,15 +81,11 @@ function convertWordToPDF(req, filePathWord, filePathPDF, ) {
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    if ('OPTIONS' == req.method) {
-       res.sendStatus(200);
-    }
-    else {
-       next();
-    }
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 });
 
 
