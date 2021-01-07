8a3d057d6796fefe87b16395e6bcdf42df449dee
Dino Karahmetovic <dino.prushka@gmail.com>
2021-01-07 16:44:08 +0100

    Commiting

---
+++ app.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const path = require('path');
+const upload = require('express-fileupload')
+const convertToPDF = require('office-to-pdf')
+const fs = require('fs');
+const nodemailer = require('nodemailer')
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(upload())
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+let dataForFiles = fs.readFileSync(path.join(__dirname, 'filesConverted', 'filesConverted.txt'), {encoding:'utf8'}); 
+
+function sendMail(mail, pathFile) {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'dkarahmetovic5@gmail.com',
+          pass: process.env.PASSWORD
+        }
+    });
+    
+    let mailOptions = {
+        from: 'dkarahmetovic5@gmail.com',
+        to: mail,
+        subject: 'Converting Word to PDF',
+        text: 'Thank you for using our services!',
+        attachments: [
+            {   
+                path: pathFile
+            }
+        ]
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    }); 
+}
+
+
+app.get('/', (req, res) => {
+
+})
+
+app.get('/api', (req, res) => {
+
+    dataForFiles = fs.readFileSync(path.join(__dirname, 'filesConverted', 'filesConverted.txt'), {encoding:'utf8'});
+
+    res.json({
+        documentsConverted: dataForFiles
+    })
+})
+
+app.post('/upload', (req, res) => {
+    console.log(req.body, req.files)
+    if(req.files && req.files.upfile) {
+        let file = req.files.upfile;
+        let name = file.name;
+        let type = file.mimetype;
+
+        file.mv(`${__dirname}/uploads/${name}`, err => {
+            if(err) {
+                console.log('Error! ', name, err)
+                app.get('/getFileLink', (req, res) => {
+                    res.status(401)
+                    res.send({"message": "Something went wrong!"})
+                })
+                res.send('Error occured')
+            } else {
+                console.log('Successfully saved', name)
+
+                let filePathPDF = `${__dirname}/uploads/${name}.pdf`
+
+                convertToPDF(fs.readFileSync(`${__dirname}/uploads/${name}`))
+                    .then(pdfBuffer => {
+                        fs.writeFileSync(filePathPDF, pdfBuffer)
+
+                        fs.writeFileSync(path.join(__dirname, 'filesConverted', 'filesConverted.txt'), +dataForFiles + 1)
+
+                        app.get('/getFileLink', (req, res) => {
+                            res.sendFile(filePathPDF)
+                        })
+                    }).catch(err => {
+                        app.get('/getFileLink', (req, res) => {
+                            res.status(401)
+                            res.send({"message": "Something went wrong!"})
+                        })
+                        console.log('ERROR')
+                })
+
+
+                if(req.body.email) {
+                    let email = req.body.email
+
+                    sendMail(email, filePathPDF)
+                }
+
+
+                res.send('Done')
+            }
+        })
+    } else {
+        console.log('No file selected!')
+        app.get('/getFileLink', (req, res) => {
+            res.status(401)
+            res.json({"message": "No file selected!"})
+        })
+        res.send()
+    }
+})
+
+
+app.listen(3000, () => console.log('Server is up on port 3000'))
---
+++ public/index.js
@@ -0,0 +1,75 @@
+let submit =  document.getElementById('submit')
+let selectedFile = document.getElementById('file')
+
+let documentsConverted = 0;
+let request = async () => {
+    const response = await fetch('http://localhost:3000/api');
+    const data = await response.json();
+    documentsConverted = data.documentsConverted ? data.documentsConverted : '0';
+    let docsConverted = document.getElementById('docsConverted');
+    docsConverted.innerText = documentsConverted;
+
+    console.log(documentsConverted)
+}
+
+request()
+
+
+selectedFile.onchange =  () => {
+    selectedFile = document.getElementById('file')
+
+    checkFileType(selectedFile.files.item(0).type, selectedFile)
+
+    document.getElementById('choosenFile').innerText = selectedFile.files.item(0).name
+}
+
+
+submit.onclick = (e) => {
+    // console.log(e)
+    // e.preventDefault()
+
+    // fetch
+
+    document.getElementById('form').classList.add('none')
+    document.getElementById('poruka').classList.remove('none')
+
+
+    let interval = setInterval(() => {
+        fetch('http://localhost:3000/getFileLink')
+            .then(data => {
+                if(data.status === 200) {
+                    clearInterval(interval)
+
+                    request()
+
+                    document.getElementById('download').classList.remove('disabled')
+                    document.getElementById('download').classList.add('enabled')
+                    document.getElementById('poruka').innerHTML = 'Your file is ready to download <a href=".">Convert again</a>'
+                }
+                if(data.status === 401) {
+                    clearInterval(interval)
+                    document.getElementById('poruka').innerHTML = '<span class="red">Something went wrong!</span> <a href=".">Try again</a>'
+                    alert('Something went wrong. Please try again!')
+                }
+            }).catch(err => {
+
+            })
+    }, 2000)
+
+    
+}
+
+
+function checkFileType(type, input) {
+    if(type !== 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+        alert('Please select Word file!')
+        input.value = ''
+    }
+}
+
+
+function close() {
+    document.querySelector('#after').addEventListener('load', function() {
+        window.close();
+    });
+}
